07d776dd669223bcd303916a34b780f2223cc0d8
Mike Korcynski <mkorcy@gmail.com>
2017-08-18 14:20:39 -0400

    fixes misspelling of accrualPolicy

--- app/models/tufts_base.rb
+++ app/models/tufts_base.rb
@@ -45,7 +45,7 @@ class TuftsBase < ActiveFedora::Base
                            :date_accepted, :date_modified, :language, :medium,
                            :provenance, :access_rights, :rights_holder,
                            :license, :replaces, :isReplacedBy, :hasFormat,
-                           :isFormatOf, :hasPart, :accruralPolicy,
+                           :isFormatOf, :hasPart, :accrualPolicy,
                            :audience, :references, :spatial]
 
   delegate_to "DCA-ADMIN", [:published_at, :edited_at, :displays], unique: true
@@ -80,7 +80,7 @@ class TuftsBase < ActiveFedora::Base
      :date_modified, :language, :type, :format, :extent, :medium, :persname, 
      :corpname, :geogname, :subject, :genre, :provenance, :rights,
      :access_rights, :rights_holder, :license, :replaces, :isReplacedBy,
-     :hasFormat, :isFormatOf, :hasPart, :isPartOf, :accruralPolicy, :audience,
+     :hasFormat, :isFormatOf, :hasPart, :isPartOf, :accrualPolicy, :audience,
      :references, :spatial, :bibliographic_citation, :temporal, :funder,
      :resolution, :bitdepth, :colorspace, :filesize]
   end
