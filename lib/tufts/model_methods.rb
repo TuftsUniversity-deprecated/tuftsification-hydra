cc609e5f26832bee670404186c3f3d72cb3fd72d
Mike Korcynski <mkorcy@gmail.com>
2018-01-09 15:41:13 -0500

    get the string not the node

--- lib/tufts/model_methods.rb
+++ lib/tufts/model_methods.rb
@@ -37,7 +37,7 @@ module Tufts
           :access_rights => detailed_datastream.access_rights,
           :subjects => subjects,
           :temporals => datastream.temporal,
-	 			  :is_parts_of => datastream.find_by_terms_and_value(:isPartOf) || []
+	 			  :is_parts_of => datastream.isPartOf
       }
     end
 
